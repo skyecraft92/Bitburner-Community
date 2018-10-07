18c5f9434b65a97d3e9ee253b0b66b46c292a1e1
Alex Lee <alex@alex-lee.site>
2018-10-06 18:31:44 -0700

    🎨 Fix Split by Focus on Right

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom'
 import * as serviceWorker from './serviceWorker'
 import App from 'grommet/components/App'
 import Split from 'grommet/components/Split'
@@ -8,7 +8,7 @@ import MainContent from './components/MainContent'
 
 ReactDOM.render(
   <App centered={false}>
-    <Split>
+    <Split flex="right">
       <MainSidebar />
       <MainContent />
     </Split>
